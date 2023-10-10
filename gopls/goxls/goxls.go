f1ff0fc27ebd914e859a0f6938bc80d02743ae64
xushiwei <x@goplus.org>
2023-10-10 20:47:52 +0800

    gopls.proxy

--- gopls/goxls/goxls.go
+++ gopls/goxls/goxls.go
@@ -1,6 +1,7 @@
 // Copyright 2022 The GoPlus Authors (goplus.org). All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
 package goxls
 
 import (
@@ -9,12 +10,10 @@ import (
 
 	"golang.org/x/tools/gopls/internal/goxls/cmd"
 	"golang.org/x/tools/gopls/internal/hooks"
-	"golang.org/x/tools/gopls/internal/telemetry"
 	"golang.org/x/tools/internal/tool"
 )
 
 func Main() {
-	telemetry.Start()
 	ctx := context.Background()
 	tool.Main(ctx, cmd.New("goxls", "", nil, hooks.Options), os.Args[1:])
 }
