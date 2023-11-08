4250a6c74b472f5d1d6678a8ca15300eaa21a0e3
xushiwei <x@goplus.org>
2023-11-08 22:54:51 +0800

    golang.org/x/tools/gop

---
+++ gop/analysis/passes/internal/analysisutil/util_test.go
@@ -0,0 +1,52 @@
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package analysisutil_test
+
+import (
+	"go/types"
+	"testing"
+
+	"github.com/goplus/gop/ast"
+	"github.com/goplus/gop/parser"
+	"github.com/goplus/gop/token"
+	"github.com/goplus/gop/x/typesutil"
+	"golang.org/x/tools/gop/analysis/passes/internal/analysisutil"
+)
+
+func TestHasSideEffects(t *testing.T) {
+	src := `package p
+
+type T int
+
+func _() {
+	var x int
+	_ = T(x)
+}
+`
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, "p.gop", src, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var conf typesutil.CheckConfig
+	info := &typesutil.Info{
+		Types: make(map[ast.Expr]types.TypeAndValue),
+	}
+	typesutil.SetDebug(typesutil.DbgFlagAll)
+	_, err = conf.Check("", fset, []*ast.File{file}, info)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ast.Inspect(file, func(node ast.Node) bool {
+		call, ok := node.(*ast.CallExpr)
+		if !ok {
+			return true
+		}
+		if got := analysisutil.HasSideEffects(info, call); got != false {
+			t.Errorf("HasSideEffects(%s) = true, want false", typesutil.ExprString(call))
+		}
+		return true
+	})
+}
